1a0b58b6a0b05a7c78f828e61f5af88a08a291bd
Khunnoodev <noo_okok@hotmail.com>
2019-12-18 16:22:18 +0700

    lab03

--- lab03/emplist.php
+++ lab03/emplist.php
@@ -8,18 +8,21 @@
     body {
         font-family: 'Alegreya';
         font-size: 16px;
+        background-color: #000000;
+        color: #FFFFFF;
     }
     </style>
 </head>
 
 <body>
-    <a href="newemp.html">New Emp</a>
+    <h1>New Emp [<a href="newemp.html">+</a>]</h1>
+   
     <?php
     // connect database 
     $db_host = "localhost";
-    $db_user = "s61160108";
-    $db_password = "212224236";
-    $db_name = "s61160108";
+    $db_user = "s61160229";
+    $db_password = "noo0912213923";
+    $db_name = "s61160229";
 
     $mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);
     $mysqli->set_charset("utf8");
@@ -32,11 +35,10 @@
     }
 
     // select data from tables
-    //echo $_GET['limit'];
     $limit = ($_GET['limit']<>"")? $_GET['limit'] : 10;
     $sql = "SELECT *
             FROM emp
-            ORDER BY 1 
+            ORDER BY 1
             LIMIT 0, $limit";
     $result = $mysqli->query($sql);
 
@@ -44,24 +46,21 @@
             echo ("Error: ". $mysqli->error);
     } else {
         $html = "<table border='1'>";
-	$html.= "<tr>";
-	$html.= "<th>Emp no</th>";
-	$html.= "<th>Name</th>";
-	$html.= "<th>Email</th>";
-	$html.= "</tr>";
+        $html.= "<tr>";
+        $html.= "<th>Emp no</th>";
+        $html.= "<th>Name</th>";
+        $html.= "<th>Email</th>";
+        $html.= "</tr>";
         while($row = $result->fetch_object()){ 
-		$html.= "<tr>";
-		$html.= "<td>";
-		$html.= "[<a href='edit.php?empno=$row->empno'>Edit</a>]";
-		$html.= "[<a href='delete.php?empno=$row->empno'>X</a>]";
-		$html.= "</td>";
-		$html.="<td>$row->empno</td>";
-		$html.="<td>$row->name</td>";
- 		$html.="<td>$row->email</td>";
-		$html.="</tr>";
+            $html.= "<tr>";
+            $html.= "<td>$row->empno</td>";
+            $html.= "<td>$row->emp_name</td>";
+            $html.= "<td>$row->emp_email</td>";
+            $html.= "</tr>";
             }
-	$html.="</table>";
-	echo $html;
+            $html.= "</table>";
+            echo $html;
     }
     ?>
-    <hr />
+</body>
+</html>
--- lab03/save.php
+++ lab03/save.php
@@ -1,27 +1,28 @@
+<meta charset="UTF-8">
 <?php
     // connect database 
     $db_host = "localhost";
-    $db_user = "s61160108";
-    $db_password = "212224236";
-    $db_name = "s61160108";
+    $db_user = "s61160229";
+    $db_password = "noo0912213923";
+    $db_name = "s61160229";
+
     $mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);
     $mysqli->set_charset("utf8");
 
-
-  //echo "<pre>";
-  //print_r($_POST);
-  //echo "</pre>";
-
-
-$empno=$_POST['empno'];
-$name=$_POST['name'];
-$email=$_POST['email'];
-$passwd=$_POST['passwd'];
-$sql = "INSERT 
-            INTO emp (empno, name,email,passwd) 
-            VALUES (?,?,?,?)";
-    $stmt = $mysqli->prepare($sql);
-    $stmt->bind_param("ssss", $empno, $name,$email,md5($passwd));
-    $stmt->execute();
-
-header("location:emplist.php");
+//    echo "<pre>";
+//     print_r($_POST);
+//     echo"</pre>";
+    $empno = $_POST['empno'];
+    $emp_name = $_POST['name'];
+    $emp_email = $_POST['email'];
+    $emp_passwd = md5($_POST['password']);
+    $sql = "INSERT 
+    INTO emp (empno, emp_name, emp_email, emp_passwd) 
+    VALUES (?, ?, ?, ?)";
+    
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("ssss", $empno, $emp_name, $emp_email, $emp_passwd);
+$stmt->execute();
+// echo $stmt->affected_rows . " row inserted.";
+header("location: emplist.php");
+?>
